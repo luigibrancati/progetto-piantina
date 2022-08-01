3b6cac23610cc84a3461b30af2fada44c0d13fa4
Luigi Brancati <luigi.brancati@flowe.com>
2022-08-01 14:34:12 +0200

    Added variable for channel

--- include/battery.h
+++ include/battery.h
@@ -4,7 +4,7 @@
 #include <esp_adc_cal.h>
 
 static const float CRITICALLY_LOW_BATTERY_VOLTAGE = 3.30;
-static const gpio_num_t BAT_DIV = GPIO_NUM_25;
+static const adc2_channel_t BAT_DIV = ADC2_CHANNEL_8; // GPIO 25 corresponds to ADC2 channel 8
 
 // Taken from https://github.com/Torxgewinde/Firebeetle-2-ESP32-E/blob/main/Firebeetle_DeepSleep.ino
 float read_battery() {
@@ -14,12 +14,12 @@ float read_battery() {
   esp_adc_cal_characteristics_t adc_chars;
 
   //battery voltage divided by 2 can be measured at GPIO25, which equals ADC2_CHANNEL8
-  adc2_config_channel_atten(ADC2_CHANNEL_8, ADC_ATTEN_DB_11);
+  adc2_config_channel_atten(BAT_DIV, ADC_ATTEN_DB_11);
   esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
 
   //to avoid noise, sample the pin several times and average the result
   for(int i=0; i<rounds; i++) {
-    adc2_get_raw(ADC2_CHANNEL_8, ADC_WIDTH_BIT_12, &raw_value);
+    adc2_get_raw(BAT_DIV, ADC_WIDTH_BIT_12, &raw_value);
     value += raw_value;
   }
   value /= (uint32_t)rounds;
